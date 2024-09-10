f8fab859ab57da19b20559d0a4e275fa87de3da2
SaktiPadmayoga <sakti14yoga@gmail.com>
2024-09-10 14:17:46 +0700

    Fix home page  mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ import { BiLogoFlutter, BiLogoTailwindCss } from "react-icons/bi";
 
 const Hero = () => {
   return (
-      <div className=" flex-col justify-center items-center min-h-screen md:min-h-[80vh] max-w-[1200px] mx-auto grid md:grid-cols-8 gap-6 p-5 mb-5 md:px-5 md:mt-3 h-full" id="home">
+      <div className=" flex-col justify-center min-h-screen md:min-h-[80vh] max-w-[1200px] mx-auto grid md:grid-cols-8 gap-6 p-5 mb-5 md:px-5 md:pt-5 md:mb-5 h-full" id="home">
       <motion.div 
         initial={{ opacity: 0, scale: 0.7 }}
         animate={{ opacity: 1, scale: 1 }}
@@ -22,21 +22,21 @@ const Hero = () => {
         exit={{ opacity: 0, x: -100, transition: { delay: 0.3, duration: 0.5, ease: "easeOut" }}}
         exitTransition={{ duration: 0.1, ease: "easeOut" }}
         
-        className="grid grid-cols-2 md:col-span-5 py-2 pl-2 pr-2 md:m-0 glass">
+        className="grid grid-cols-2 md:col-span-5 p-5 sm:my-2 glass">
         <img className="w-[400px] " src={profilepic} alt="Profile Pic" />
-        <div className="my-auto ml-2 flex-col text-gray-200">
-          <p className="text-lg md:text-3xl font-bold py-1">
+        <div className="my-auto ml-6 flex-col text-gray-200">
+          <p className="text-lg sm:text-3xl font-bold py-1">
             Hi Fellas!
             <br /> I am Sakti Padmayoga
-            <div className="text-base md:text-2xl">
+            <div className="text-base sm:text-2xl">
             a/an <br className="sm:hidden"/>
               <TypeAnimation sequence={[" Web Developer", 1000, " Software Developer", 1000, " UI Designer", 1000]} wrapper="span" speed={50} repeat={Infinity} />  
             </div>
             
           </p>
-          <p className="text-lg md:text:2xl text-gray-400 font-semibold mt-2 hidden">with 0 year of experience :D.</p>
-          <button className="mt-4 px-4 py-2 text-sm font-bold text-gray-200 bg-primary-color rounded-lg ">Download CV  💌</button>
-          <button className="mt-4 md:ml-3 px-4 py-2 text-sm font-bold text-gray-200 bg-zinc-800 rounded-lg hover:bg-slate-600 transition duration-300">
+          <p className="hidden text-lg md:text:2xl text-gray-400 font-semibold mt-2 sm:">with 0 year of experience :D.</p>
+          <button className="mt-4 px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm font-bold text-gray-200 bg-primary-color rounded-lg ">Download CV  💌</button>
+          <button className="mt-4 lg:ml-3 px-2 py-1 text-xs sm:px-4 sm:py-2 sm:text-sm  font-bold text-gray-200 bg-zinc-800 rounded-lg hover:bg-slate-600 transition duration-300">
             <a href="/about">About Me →</a>
           </button>
         </div>
