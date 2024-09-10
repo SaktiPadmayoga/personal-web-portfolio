6e32dff94d1db80f5ed7cf59d9157cb3adb93cfe
SaktiPadmayoga <sakti14yoga@gmail.com>
2024-09-10 12:01:54 +0700

    adjust the code so that there are no 2 routes in 1 program

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
 import Footer from "./components/Footer";
-import Education from "./components/Education";
-import Work from "./components/Work";
-import Contact from "./components/Contact";
+import AnimatedRoutes from './components/AnimatedRoutes';
 
-function App() {
+function App  () {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero/>} />
-        <Route path="/about" element={
-          <>
-            <About />
-            <Education />
-          </>
-        } />
-        <Route path="/skills" element={<Skills/>} />
-        <Route path="/work" element={<Work/>} />
-        <Route path="/contact" element={<Contact/>} />
-      </Routes>
+      <AnimatedRoutes/>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
